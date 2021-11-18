e0c194ba015c2f57bac7cdbb9de0a79161ce6095
elasri21 <islame.edu@gmail.com>
2021-11-18 13:12:17 +0100

    comments were added to js file

--- tab.js
+++ tab.js
@@ -1,17 +1,17 @@
-const titles = document.querySelectorAll(".title")
-const boxes = document.querySelectorAll(".box");
+const titles = document.querySelectorAll(".title") // get all buttons 
+const boxes = document.querySelectorAll(".box");  // get boxes
 
 
-titles.forEach(btn => {
-    btn.addEventListener("click", function(){
+titles.forEach(btn => {  
+    btn.addEventListener("click", function(){  // add a click event to each button
         for(let j = 0; j< titles.length;j++){
-            titles[j].classList.remove("active")
+            titles[j].classList.remove("active")  // remove active class from all buttons
         }
         for(let i = 0;i< boxes.length;i++){
             boxes[i].classList.remove("active")
-            if(this.dataset.index == boxes[i].dataset.index) {
-                boxes[i].classList.add("active")
-                this.classList.add("active")
+            if(this.dataset.index == boxes[i].dataset.index) { 
+                boxes[i].classList.add("active")  // add active class to the target boxes
+                this.classList.add("active")  // add active class to the target button
             }
         }  
     })
